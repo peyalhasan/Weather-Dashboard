3e1a635d31b08c1dc022dea8201284a0cfe9415b
Peyal Hasan <peyalhasan143@gmail.com>
2025-12-06 14:32:05 +0600

    Weather Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Components/Header/Header"
+import WeatherBoard from "./Components/Weather/WeatherBoard"
+
+
+function App() {
+
+  return (
+    <div className="grid place-items-center h-screen">
+      <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Favourite.jsx
@@ -0,0 +1,11 @@
+import HeartIcon from '../../assets/heart.svg'
+const Favourite = () => {
+    return (
+        <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+            <img src={HeartIcon} alt="" />
+            <span>Favourite Locations</span>
+        </div>
+    );
+};
+
+export default Favourite;
